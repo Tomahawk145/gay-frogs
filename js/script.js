72112f8e6fe09aa1fd4d82ce0116f7386bb317f6
Zac Lucarelli <chikanz@outlook.com>
2018-09-08 21:53:31 +1000

    Added in siema + swipe animation
    Body text now changes on siema slide
    -text is loaded from a json now though, and chrome can't load files locally so I'll have to sort out that sheet
    Added in body fonts from infowars
    Organised files a litte

--- js/script.js
+++ js/script.js
@@ -1,48 +1,88 @@
 
-
-var navLinks = document.querySelectorAll( 'nav a' );
-
+//Get all nav links
+var navLinks = document.querySelectorAll('nav a');
 var scrollOffset = 0;
 
 animateScroll();
 
-imagesLoaded( 'body', { background: 'section' }, 
-			 function() {
-	document.querySelector('#loading').className = 'loaded';
-	setTimeout (function() {
-		AOS.init( {duration: 1000});
-	}, 1000);
-} );
+imagesLoaded('body', { background: 'section' },
+	function () {
+		document.querySelector('#loading').className = 'loaded';
+		setTimeout(function () {
+			AOS.init({ duration: 1000 });
+		}, 1000);
+	});
 
-for (var i= 0; i< navLinks.length; i++){
+for (var i = 0; i < navLinks.length; i++) {
 	navLinks[i].addEventListener('click', selectLink);
 	navLinks[i].addEventListener('click', setScrollOffset);
 }
 
 function selectLink() {
-	for (var i = 0; i < navLinks.length; i++){
+	for (var i = 0; i < navLinks.length; i++) {
 		navLinks[i].className = '';
 	}
 	this.className = 'selected';
 }
 
-function setScrollOffset ( event ) {
+function setScrollOffset(event) {
 	event.preventDefault();
 	var section = document.querySelector(this.hash);
 	scrollOffset = section.offsetTop;
-	requestAnimationFrame( animateScroll );
+	requestAnimationFrame(animateScroll);
 }
 
 function animateScroll() {
-	var scrollPosition = document.documentElement.scrollTop||document.body.scrollTop;
-	var scrollDistance = Math.round( scrollOffset - scrollPosition);
+	var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+	var scrollDistance = Math.round(scrollOffset - scrollPosition);
 	if (scrollDistance > 0) {
-		scrollPosition += Math.ceil( scrollDistance / 10);
+		scrollPosition += Math.ceil(scrollDistance / 10);
 		requestAnimationFrame(animateScroll);
-	} else if (scrollDistance < 0){	
-		scrollPosition += Math.floor( scrollDistance / 10);
-		requestAnimationFrame( animateScroll); 
-	} 
+	} else if (scrollDistance < 0) {
+		scrollPosition += Math.floor(scrollDistance / 10);
+		requestAnimationFrame(animateScroll);
+	}
 	document.documentElement.scrollTop = scrollPosition;
 	document.body.scrollTop = scrollPosition;
+}
+
+
+//Siema stuffff
+const mySiema = new Siema({
+	onChange: onChangeCallback,
+});
+
+
+
+//Load in description data from file async
+//Since chrome is a tight ass and can't load json from a local file, this needs to be hosted somewhere
+//So if you're running locally on a server, 
+var data = "";
+$.getJSON("js/Page3Descriptions.json", function(json) {
+	data = json;
+	updateCaptions(0);
+});
+
+//daddy javascript... I don't feel so good uwu
+var removed = false; //Flag to only trigger once
+function onChangeCallback() {
+	if (!removed) {
+		removed = true;
+		var hand = document.querySelector('.pointer');
+		hand.classList.add('fade');
+
+		setTimeout(() => {
+			hand.remove();
+		}, 4000); //delayed remove after fade for E F F I C I E N C Y
+	}
+
+	//Update captions
+	updateCaptions(this.currentSlide);
+}
+
+function updateCaptions(i)
+{
+	//Update captions
+	document.querySelector('.captionTitle').innerHTML = data[i].title;
+	document.querySelector('.captionBody').innerHTML = data[i].body;
 }
