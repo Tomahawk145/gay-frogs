62a244cb1bff4b7a08fbb7f63f55e53b1ae34a6c
Zac Lucarelli <chikanz@outlook.com>
2018-09-13 15:46:20 +1000

    Finito :tada:
    cleaned up files a lil
    Added final froggy anims
    cleaned up meme code comments

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
-
-
 var scrollOffset = 0;
 animateScroll();
 
 //load in audio
 var audio = new Audio('audio/bangers.mp3');
 
-function stopAudio()
-{
+function stopAudio() {
 	audio.pause();
 	audio.currentTime = 0;
 }
@@ -25,15 +22,16 @@ for (var i = 0; i < navLinks.length; i++) {
 	navLinks[i].addEventListener('click', setScrollOffset);
 }
 
-function selectLink() {
-	var index = -1;
+var index = -1; //global var for page ref
+function selectLink() 
+{
 	for (var i = 0; i < navLinks.length; i++) {
 		if (navLinks[i] == this) index = i; //find where me is
 		navLinks[i].className = '';
 	}
 	this.className = 'selected';
 
-	//Play audio or don't
+	//Only play audio on page 4
 	index == 4 ? audio.play() : stopAudio();
 
 	//First page anims
@@ -53,28 +51,63 @@ function selectLink() {
 	}
 
 	//Doof doof
-	if(index == 4)
-	{
+	if (index == 4) {
 		BPM = 125;
-		setInterval(doof, 1000 * 60 / 125);
+		document.querySelector('.speaker').classList.add('doof');
+		setInterval(doof, 1000 * 60 / 125); //make speakers doof
+		var frogs = document.querySelectorAll('.frog');
+		frogLoop(frogs[0]);
+		frogLoop(frogs[1]);
 	}
-	else
-	{
-		clearInterval(); //sthawp
+	else {
+		clearInterval(); //stopppp
 		document.querySelector('.speaker').classList.remove('doof');
 	}
 }
 
-function doof()
+function doof() 
 {
-	document.querySelector('.speaker').AnimationPlayState
+	//Basically, iterate each speaker with jquery, then restart its css animation by 
+	//replacing the original with a clone
+	//https://css-tricks.com/restart-css-animation/
+	$(".speaker").each(
+		function(index)
+		{
+			resetAnim(this);
+		}
+	);
 }
 
-function stopDoof()
+//man js is shite
+function randomhelper(min, max)
 {
-
+	return Math.random() * (+max - +min) + +min;
 }
 
+//Recursively call self to set random timer interval
+//Since resetting the animation makes our reference invalid, we get the new ref from resetanim 
+//https://stackoverflow.com/questions/6962658/randomize-setinterval-how-to-rewrite-same-random-after-random-interval
+function frogLoop(frog)
+{
+	if(index != 4) return; //Exit out of recursive loop if not on froggy page
+	var rand = randomhelper(800,1200);
+    setTimeout(function() {
+		frog = resetAnim(frog);
+            frogLoop(frog);  
+    }, rand);
+}
+
+
+//clone and delet to restart anim
+function resetAnim(elm)
+{
+	var newone = elm.cloneNode(true);
+	elm.parentNode.replaceChild(newone, elm);
+	return newone;
+}
+
+
+//Scrolly easing stuff from lab
 function setScrollOffset(event) {
 	event.preventDefault();
 	var section = document.querySelector(this.hash);
@@ -96,8 +129,7 @@ function animateScroll() {
 	document.body.scrollTop = scrollPosition;
 }
 
-
-//Siema stuffff
+//Courasel stuff
 const mySiema = new Siema({
 	onChange: onChangeCallback,
 });
@@ -115,7 +147,7 @@ $.getJSON(jsonUrl, function (json) {
 	updateCaptions(0);
 });
 
-//daddy javascript... I don't feel so good uwu
+//Fade out hand anim on siema slide changed
 var removed = false; //Flag to only trigger once
 function onChangeCallback() {
 	if (!removed) {
@@ -128,22 +160,20 @@ function onChangeCallback() {
 		}, 4000); //delayed remove after fade for E F F I C I E N C Y
 	}
 
-	//Update captions
+	//Change caption text
 	updateCaptions(this.currentSlide);
 
 	//Update the background with blurred slide images
 	updateBG(this.currentSlide)
 }
 
-function updateCaptions(i) 
-{
+function updateCaptions(i) {
 	document.querySelector('.captionTitle').innerHTML = data[i].title;
 	document.querySelector('.captionBody').innerHTML = data[i].body;
 }
 
-function updateBG(i)
-{	
-	var img = "url(css/" + (i+1) +".jpg" + ")";	
+function updateBG(i) {
+	var img = "url(css/" + (i + 1) + ".jpg" + ")";
 	document.getElementById("BGChange").style.backgroundImage = img;
 }
 
@@ -151,29 +181,25 @@ function updateBG(i)
 //Text validation
 $('#message').on('input', remaining); //Event to call function every keypress
 $(document).ready(remaining) //Also call once after the page is ready to get size + set color
-function remaining() 
-{
+function remaining() {
 	//Find out how many letters are remaining and show user
 	var me = document.querySelector('#message');
-    var remain = me.maxLength - me.value.length; 
-	$('#remaining').text(remain);
-	
+	var remain = me.maxLength - me.value.length;
+	$('#remaining').text(remain); //Set text
+
 	//Set to red or blue depending on if the amount is valid
 	$('#remaining').css('color', remain <= 0 ? 'red' : '#263350');
 }
 
-//Text submission
+//send to twitter
 $('#twitter').on('submit', checkForm); //Event to call function on click
-function checkForm(event) 
-{
-    event.preventDefault(); //Stop default behevaiour
-    var twit = $('#message').val();
-    tweet = encodeURIComponent(twit); //Filter out any special characters that might stuff up the URL
-	if (twit.length > 0)
-	{
-        window.open('https://twitter.com/intent/tweet?text=' + tweet, '', 'top="500",left="500",menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600')
-	} else 
-	{
-        alert("Your tweet can't be empty! Or is that just what the Illuminati wants you to think????")
-    }
+function checkForm(event) {
+	event.preventDefault(); //Stop default behevaiour
+	var twit = $('#message').val(); //Get message from input box
+	tweet = encodeURIComponent(twit); //Filter out any special characters that might stuff up the URL
+	if (twit.length > 0) {
+		window.open('https://twitter.com/intent/tweet?text=' + tweet, '', 'top="500",left="500",menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600')
+	} else {
+		alert("Your tweet can't be empty! Or is that just what the Illuminati wants you to think????")
+	}
 }
